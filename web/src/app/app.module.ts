412270fbfd010b804dbd0247f59af267cd0876f4
Mdelaf <mdelafuente1@uc.cl>
2021-07-04 13:06:32 -0400

    feat(web): implement web skeleton

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PitchDeckComponent } from './components/pitch-deck/pitch-deck.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PitchDeckComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/src/app/components/pitch-deck/pitch-deck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pitch-deck',
+  templateUrl: './pitch-deck.component.html',
+  styleUrls: ['./pitch-deck.component.scss']
+})
+export class PitchDeckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
